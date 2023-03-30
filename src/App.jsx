5e59ac237b3879a73f5b32028f5acd39a842f12d
SadmanJawad <jawadnabil977@gmail.com>
2023-03-30 16:08:53 +0600

    cart add dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import Contact from './components/Contact/Contact';
+import SideCart from './components/SideCart/SideCart';
+import Home from './components/Home/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-
+    <div>
+      <div className='header m-auto mb-3'>
+        <Header></Header>
+      </div>
+      <div className="main row">
+        <div className="home-container col-md-8">
+          <Home></Home>
+        </div>
+        <div className="sideCart col-md-4 card">
+          <SideCart></SideCart>
+        </div>
+      </div>
+      <Contact></Contact>
     </div>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import SingleCard from "../SingleCard/SingleCard";
+
+const Home = () => {
+    const [movies, setMovies] = useState([])
+    useEffect(()=> {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setMovies(data))
+    },[])
+
+
+
+
+  return (
+    <div>
+      <div className="movie-container row">
+        {movies.map((movie) => (
+                <SingleCard movie= {movie}></SingleCard>
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
